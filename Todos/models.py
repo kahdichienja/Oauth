3eadda10a23cbd6166cebd6a865c2d395964921b
kahdichienja <agooclinton@gmail.com>
2019-02-22 17:18:36 +0300

    models

--- Todos/models.py
+++ Todos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Todos(models.Model):
+    # id          = models.AutoField(primary_key=True)
+    name        = models.CharField(max_length=30)
+    Task        = models.TextField(max_length=30)
+    date        = models.DateTimeField(default = datetime.now, blank = True)
+    is_complete = models.CharField(max_length = 5, default=True, blank = True)
