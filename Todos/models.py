539b0c9b94138b4ec08f04ef15e13656b4f68ea0
kahdichienja <agooclinton@gmail.com>
2019-02-23 10:37:13 +0300

    adding todos list view and id

--- Todos/models.py
+++ Todos/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class Todos(models.Model):
     # id          = models.AutoField(primary_key=True)
     name        = models.CharField(max_length=30)
-    Task        = models.TextField(max_length=30)
+    Task        = models.TextField(max_length=200)
     date        = models.DateTimeField(default = datetime.now, blank = True)
     is_complete = models.CharField(max_length = 5, default=True, blank = True)
 
--- Todos/urls.py
+++ Todos/urls.py
@@ -5,5 +5,5 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('details/<id>/', views.details)
+    path('todos/details/<id>/', views.details)
 ]
